2d2dd8205d51daba3ab9fab28240a83ad552f39f
dpyzo0o <dpyzo0o@outlook.com>
2020-07-17 11:40:09 +0800

    clean up

--- next.config.js
+++ next.config.js
@@ -1,76 +1,8 @@
-const path = require('path')
+const withOptimizedImages = require('next-optimized-images')
+const withMDX = require('./src/libs/withMDX')
 
-module.exports = {
-  pageExtensions: ['mdx', 'ts', 'tsx'],
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
-          },
-        },
-      ],
-    })
-
-    config.module.rules.push({
-      test: /\.mdx$/,
-      oneOf: [
-        {
-          resourceQuery: /excerpt/,
-          use: [
-            options.defaultLoaders.babel,
-            '@mdx-js/loader',
-            {
-              loader: path.resolve(
-                __dirname,
-                './src/loaders/excerpt-loader.js'
-              ),
-            },
-            {
-              loader: path.resolve(__dirname, './src/loaders/fm-loader.js'),
-            },
-          ],
-        },
-        {
-          use: [
-            options.defaultLoaders.babel,
-            {
-              loader: '@mdx-js/loader',
-              options: {
-                remarkPlugins: [
-                  // add anchor to heading
-                  require('remark-slug'),
-                  [
-                    require('remark-autolink-headings'),
-                    {
-                      behavior: 'append',
-                      linkProperties: {
-                        class: ['anchor'],
-                      },
-                      content: {
-                        type: 'text',
-                        value: '#',
-                      },
-                    },
-                  ],
-                ],
-              },
-            },
-            {
-              loader: path.resolve(__dirname, './src/loaders/mdx-loader.js'),
-            },
-            {
-              loader: path.resolve(__dirname, './src/loaders/fm-loader.js'),
-            },
-          ],
-        },
-      ],
-    })
-
-    return config
-  },
-}
+module.exports = withOptimizedImages(
+  withMDX({
+    pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+  })
+)
--- src/utils/getAllPosts.ts
+++ src/utils/getAllPosts.ts
@@ -2,7 +2,7 @@ import { FrontMatter } from 'src/types'
 
 function importAll(r: __WebpackModuleApi.RequireContext) {
   return r.keys().map(fileName => ({
-    link: fileName.substr(1).replace(/\/index\.mdx$/, ''),
+    link: fileName.substr(1).replace(/\/index\.(md|mdx)$/, ''),
     module: r<{
       frontMatter: FrontMatter
       hasExcerpt: boolean
@@ -12,7 +12,9 @@ function importAll(r: __WebpackModuleApi.RequireContext) {
 }
 
 export default function getAllPosts() {
-  return importAll(require.context('../pages/?excerpt', true, /\.mdx$/)).sort(
+  return importAll(
+    require.context('../pages/?excerpt', true, /\.(md|mdx)$/)
+  ).sort(
     (a, b) =>
       b.module.frontMatter.date.getTime() - a.module.frontMatter.date.getTime()
   )

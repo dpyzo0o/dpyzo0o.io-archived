2b9157b30470735e552e38a12d2355c736084ed2
dpyzo0o <dpyzo0o@outlook.com>
2020-08-01 14:08:05 +0800

    animate heart

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 function Footer() {
   return (
     <footer className="absolute bottom-0 w-full flex items-center justify-center text-gray-400 h-12 sm:h-16">
-      Made with&nbsp;<span className="text-red-600">&#9829;</span>&nbsp;by
-      dpyzo0o
+      Made with&nbsp;
+      <span className="text-red-600 mx-1 animate-ping">&#9829;</span>
+      &nbsp;by dpyzo0o
     </footer>
   )
 }
